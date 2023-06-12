f4003abd696fa5e19ac99811504d749894db620a
MrWesternWood <103273705+MrWesternWood@users.noreply.github.com>
2023-06-12 21:24:37 +0800

    Update parse.cpp
    完成了print_c函数的实现

--- parse.cpp
+++ parse.cpp
@@ -2,13 +2,26 @@
     实现parse.h中定义的函数
 */
 #include "parse.h"
-#include "getopt.h"
+#include "getopt.h"         //我们利用一下IBM的命令行输入框架来进行输入的解释
 #pragma warning(disable : 4996)
 
 // 需要的数独终盘数量
 void print_c(int argc, char *argv[])
 {
+    Shudu player;
+    vector<int> range;
+    ofstream outfile;
 
+    int num = atoi(argv[2]);
+    if (num < 1 || num > 1000000)
+    {
+        printf("生成数独终盘数量范围在1~1000000之间\n");
+        exit(0);
+    }
+    outfile.open("game.txt", ios::out | ios::trunc);
+    range.push_back(0);
+    generateGame(atoi(argv[2]), 0, range, outfile, player);
+    range.clear();
 }
 
 // 需要解的数独棋盘文件路径
