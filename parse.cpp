5bdf87be059fb25953cac0b416b79c6e560963ba
MrWesternWood <103273705+MrWesternWood@users.noreply.github.com>
2023-06-16 16:10:14 +0800

    实现 parse.cpp 中大部分代码
    还有一个参数解析未完成

--- parse.cpp
+++ parse.cpp
@@ -27,13 +27,104 @@ void print_c(int argc, char *argv[])
 // 需要解的数独棋盘文件路径
 void print_s(int argc, char *argv[])
 {
+    Shudu player;
+    ofstream outfile;
+    vector<Board> boards;
 
+    if (access(argv[2], 0) == -1)
+    {
+        printf("file does not exist\n");
+        exit(0);
+    }
+    outfile.open("sudoku.txt", ios::out | ios::trunc);
+    string filePath = argv[2];
+    boards = readFile(filePath);
+    for (int i = 0; i < boards.size(); i++)
+    {
+        vector<Board> result = player.solveBoard(boards[i]);
+        writeFile(result, outfile);
+    }
+    outfile.close();
 }
 
 // 需要的游戏数量
 void print_n(int argc, char *argv[])
 {
+    Shudu player;
+    map<char, string> params = parse(argc, argv);
+    map<char, string>::iterator it, tmp;
+    vector<int> range;
+    ofstream outfile;
+    int gameNumber;
+    int gameLevel = 0;
+    int solution_count = 0;
+    it = params.begin();
+    while (it != params.end())
+    {
+        tmp = params.find('n');
+        if (tmp == params.end())
+        {
+            printf("缺少参数 n \n");
+            exit(0);
+        }
 
+        gameNumber = atoi(tmp->second.c_str());
+
+        tmp = params.find('u');
+        if (tmp != params.end())
+        {
+            solution_count = 1;
+        }
+
+        tmp = params.find('m');
+        if (tmp != params.end())
+        {
+            gameLevel = atoi(tmp->second.c_str());
+        }
+
+        tmp = params.find('r');
+        if (tmp != params.end())
+        {
+            char* p;
+            char* pc = new char[100];
+            strcpy(pc, tmp->second.c_str());
+            p = strtok(pc, "~");
+            while (p)
+            {
+                range.push_back(atoi(p));
+                p = strtok(NULL, "~");
+            }
+        }
+        else
+        {
+            // 根据不同级别采取挖空数量不同
+            if (gameLevel == 1)
+            {
+                range.push_back(20);
+                range.push_back(30);
+            }
+            else if (gameLevel == 2)
+            {
+                range.push_back(30);
+                range.push_back(40);
+            }
+            else if (gameLevel == 3)
+            {
+                range.push_back(40);
+                range.push_back(55);
+            }
+            else
+            {
+                range.push_back(20);
+                range.push_back(55);
+            }
+        }
+        outfile.open("game.txt", ios::out | ios::trunc);
+        generateGame(gameNumber, gameLevel, range, outfile, player);
+        range.clear();
+        // cout << it->first << ' ' << it->second << endl;
+        it++;
+    }
 }
 
 // 生成游戏的难度
