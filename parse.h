73169675ba3672265cf3fa8e22a713e0912194e0
MrWesternWood <103273705+MrWesternWood@users.noreply.github.com>
2023-06-16 16:11:33 +0800

    添加了一个解析输入参数的声明
    其余保持不变

--- parse.h
+++ parse.h
@@ -3,6 +3,8 @@
 */
 #include "helpFunc.h"
 
+map<char, string> parse(int argc, char *argv[]);//解析输入参数
+
 void print_c(int argc, char *argv[]);//需要的数独终盘数量
 void print_s(int argc, char *argv[]);//需要解的数独棋盘文件路径
 void print_n(int argc, char *argv[]);//需要的游戏数量
